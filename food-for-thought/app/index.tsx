09f999c80179e1bf7b8c6677f2a9d9d2df32eb60
abrarbarbhuyia <abrar.barbhuyia@gmail.com>
2024-09-01 21:37:14 +1000

    feat: create new page with routing for ai testing

--- food-for-thought/app/index.tsx
+++ food-for-thought/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Home page</Text>
+      <Link href="/testing/ai">GO TO TESTING</Link>
     </View>
   );
 }

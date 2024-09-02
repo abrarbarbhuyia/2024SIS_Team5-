1bc0dc23a804cd81809b40a531dda315319be8f9
jvqk <jack.w.marshall@student.uts.edu.au>
2024-09-02 23:49:21 +1000

    Header component working

--- food-for-thought/app/_layout.tsx
+++ food-for-thought/app/_layout.tsx
@@ -3,9 +3,9 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{flex: 1,}}>
       <Stack>
-        <Stack.Screen name="index" />
+        <Stack.Screen name="index" options={{headerShown: false}}/>
         <Stack.Screen name="map" />
         {/* options={{headerShown:false}} */}
       </Stack>
--- food-for-thought/app/index.tsx
+++ food-for-thought/app/index.tsx
@@ -1,16 +1,20 @@
-import { Text, View } from "react-native";
-import { SearchBar } from '@/components/SearchBar';
+import { Text, View, Button, Dimensions } from "react-native";
+//import { SearchBar } from "../components/SearchBar";
 import { Link } from "expo-router";
+import Header from "@/components/Header";
 
 export default function Index() {
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#E6D7FA",
       }}
     >
+      <Header></Header>
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Link href={"/map"}>Navigate to Map Display</Link>
     </View>

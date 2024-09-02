3abc8f8f814cdebffadb49c50efc848c9eee8859
Georgette Bashour <13901057@student.uts.edu.au>
2024-09-02 19:46:23 +1000

    Added draggable bottom sheet view on Map page.
    Co-authored-by: Zi-Mei Alice Cui <79515543+azchu@users.noreply.github.com>

--- food-for-thought/app/_layout.tsx
+++ food-for-thought/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="map" />
-    </Stack>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="map" />
+        {/* options={{headerShown:false}} */}
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
--- food-for-thought/app/map.tsx
+++ food-for-thought/app/map.tsx
@@ -1,17 +1,61 @@
-import SearchBar from "@/components/SearchBar";
-import { Text, View } from "react-native";
+import React, { useCallback, useMemo, useRef } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
 
-export default function Map() {
+const Map = () => {
+  // ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  // renders
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <SearchBar/>
-    </View>
+    <BottomSheetModalProvider>
+      <View style={styles.container}>
+        <Button
+          onPress={handlePresentModalPress}
+          title="Present Modal"
+          color="black"
+        />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}
+        >
+          <BottomSheetView style={styles.contentContainer}>
+            <Text>Awesome 🎉</Text>
+          </BottomSheetView>
+        </BottomSheetModal>
+      </View>
+    </BottomSheetModalProvider>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    // backgroundColor: 'grey',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+
+export default Map;

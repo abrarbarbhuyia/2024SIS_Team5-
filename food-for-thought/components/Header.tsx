e4545fe4a16f12692e66bc04f41522b59fcffd33
jvqk <jack.w.marshall@student.uts.edu.au>
2024-09-03 01:06:47 +1000

    Home page cards and base styling done with POC carousel

--- food-for-thought/components/Header.tsx
+++ food-for-thought/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Card, Text, Button, Icon } from '@rneui/themed';
-import { View, ScrollView, StyleSheet, Dimensions, Image } from "react-native";
+import { Card, Text, Icon } from '@rneui/themed';
+import { View, StyleSheet, Dimensions, Image } from "react-native";
 import React from 'react';
 import logo from '../assets/images/logo.png';
 
@@ -11,26 +11,24 @@ export default function Header() {
     }
 
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <Card containerStyle={styles.headerCard}>
-                    <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 35, marginTop: 7 }}>
-                            <Icon style={{ color: '#000000' }} name='user' type='evilicon' size={35} onPress={() => console.log("Profile click")}/>
-                            <Text style={{ marginLeft: 8 }}>Nep</Text>
-                        </View>
-                        {/* <Icon name='user' type='evilicon' /> */}
-                        {/* also some text alongside the icon */}
-                        <Card.Image source={logo} style={styles.image} onPress={handleHomePress}/>
-                        {/* now two more icons on other side of logo, side by side */}
-                        <View style={{ flexDirection: 'row', marginLeft: 35, marginTop: 10}}>
-                            <Icon style={{ color: '#000000', marginRight: 20 }} name='bell' type='fontisto' size={25} onPress={() => console.log("Notifications click")}/>
-                            <Icon style={{ color: '#000000' }} name='spinner-cog' type='fontisto' size={25} onPress={() => console.log("Settings click")}/>
-                        </View>
+        <View style={styles.container}>
+            <Card containerStyle={styles.headerCard}>
+                <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 35, marginTop: 7 }}>
+                        <Icon style={{ color: '#000000' }} name='user' type='evilicon' size={35} onPress={() => console.log("Profile click")}/>
+                        <Text style={{ marginLeft: 8 }}>Nep</Text>
                     </View>
-                </Card>
-            </View>
-        </ScrollView>
+                    {/* <Icon name='user' type='evilicon' /> */}
+                    {/* also some text alongside the icon */}
+                    <Card.Image source={logo} style={styles.image} onPress={handleHomePress}/>
+                    {/* now two more icons on other side of logo, side by side */}
+                    <View style={{ flexDirection: 'row', marginLeft: 35, marginTop: 10}}>
+                        <Icon style={{ color: '#000000', marginRight: 20 }} name='bell' type='fontisto' size={25} onPress={() => console.log("Notifications click")}/>
+                        <Icon style={{ color: '#000000' }} name='spinner-cog' type='fontisto' size={25} onPress={() => console.log("Settings click")}/>
+                    </View>
+                </View>
+            </Card>
+        </View>
     );
 }
 
@@ -44,7 +42,7 @@ const styles = StyleSheet.create({
     headerCard: {
         width: width - 32,
         height: 100,
-        backgroundColor: "white",
+        backgroundColor: "#FBF8FF",
         padding: 12,
         borderRadius: 24,
         marginTop: 40,
@@ -52,7 +50,6 @@ const styles = StyleSheet.create({
         shadowOffset: {width: 0, height: 2},
         shadowOpacity: 2,
         shadowRadius: 4,
-        marginBottom: 16,
         justifyContent: "space-between",
     },
     image: {

a4bf3f40822a3c1b066614638f431deeb5e784cb
abrarbarbhuyia <abrar.barbhuyia@gmail.com>
2024-09-11 15:06:42 +1000

    feat: implement gemini api call for menus, return JSON for calls, create accessible gemini functions
    Co-authored-by: Adam Nasser <79546879+AdamNas123@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 const {testConnection} = require('./databaseMaster');
-const {testSuggestic, testGemini} = require('./allergenMaster');
+const {testSuggestic, getIngredientDetails, getMeals} = require('./allergenMaster');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,5 +12,6 @@ app.listen(port, async () => {
   console.log(`Example app listening on port ${port}`)
   await testConnection();
   // await testSuggestic("STEAM BBQ BUN");
-  await testGemini("Pad see ew");
+  console.log(await getMeals("FOOD MENU Paucek and Lage Restaurant MAIN COURSE Cheeseburger — $34 Cheese sandwich — $22 Chicken burgers — $23 Spicy chicken — $33 Hot dog — $24 APPETIZERS Fruit Salad — $13 Cocktails — $12 Nuggets — $14 Sandwich — $13 French Fries — $15 BEVERAGES Milk Shake — $3 Iced Tea — $2 Orange Juice — $4 Lemon Tea — $3 Coffee — $5 123-456-7890 123 Anywhere St., Any City"));
+  console.log(await getIngredientDetails("Pad see ew"));
 })

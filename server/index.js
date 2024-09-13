e5c825135e777718ebd058df5e0c79679880cc74
Niyumi Jayawardane <niyumijaya@outlook.com>
2024-09-13 15:46:35 +1000

    moved the getMenuString() call to server/routes/menu.js Co-authored-by: Tara Kenisha Uy <92384503+tarakenishauy@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -3,19 +3,11 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 const { testConnection } = require('./databaseMaster');
-const { getMenu } = require('./menu');
 const { testSuggestic, getIngredientDetails, getMeals } = require('./allergenMaster');
+const menuRoutes = require('./routes/menu');
 
 app.use(cors());
-
-app.get('/api/menu', async (req, res) => {
-  try {
-    const menuImage = await getMenu();
-    res.json(menuImage);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+app.use('/menu', menuRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');

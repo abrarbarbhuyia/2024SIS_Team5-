50b53ab2f349a2991cbe90eca0eeb025d5a318eb
Z <13918317@student.uts.edu.au>
2024-09-13 21:17:05 +1000

    Registration works now

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-const User = require('../models/userModel');
-const router = express.Router();
+const { dbOp } = require('../databaseMaster');
 const bcrypt = require('bcryptjs');
+const router = express.Router();
 
 router.post('/', [
     body('username').not().isEmpty().withMessage('Username is required'),
-    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters').matches(/[!@#$%^&*]/).withMessage('Password must contain at least one special character')
 ], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
+    const validationFailures = validationResult(req);
+    if (!validationFailures.isEmpty()) {
+        return res.status(400).json({ validationFailures: validationFailures.array() });
     }
 
     const { username, password } = req.body;
 
     try {
-        let user = await User.findOne({ username });
-        if (user) {
-            res.status(400).json({ message: `${username} already exists` });
+        const existingUser = await dbOp('find', 'User', { query: { username } });
+        if (existingUser.length > 0) {
+            return res.status(400).json({ message: `User ${username} already exists.` });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        user = new User({ username, password: hashedPassword });
-        await user.save();
+        const newUser = { username, password: hashedPassword };
+        await dbOp('insert', 'User', { docs: [newUser] });
 
-        res.status(201).json({ message: `${username} registered successfully`});
+        res.status(201).json({ message: `User ${username} registered successfully.` });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }

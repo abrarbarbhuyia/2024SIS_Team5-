c8594c5eaf6c9423fd42f576857e5c9309d00a1d
Niyumi Jayawardane <niyumijaya@outlook.com>
2024-09-13 15:11:44 +1000

    test if coauthoring works
    Co-authored-by: Tara Kenisha Uy <92384503+tarakenishauy@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -27,5 +27,4 @@ app.listen(port, async () => {
    // await testSuggestic("STEAM BBQ BUN");
    console.log(await getMeals("FOOD MENU Paucek and Lage Restaurant MAIN COURSE Cheeseburger — $34 Cheese sandwich — $22 Chicken burgers — $23 Spicy chicken — $33 Hot dog — $24 APPETIZERS Fruit Salad — $13 Cocktails — $12 Nuggets — $14 Sandwich — $13 French Fries — $15 BEVERAGES Milk Shake — $3 Iced Tea — $2 Orange Juice — $4 Lemon Tea — $3 Coffee — $5 123-456-7890 123 Anywhere St., Any City"));
    console.log(await getIngredientDetails("Pad see ew"));
-  await getMenu();
 });

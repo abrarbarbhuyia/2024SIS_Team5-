2a6f37b317f1344038fe85e0d949684f39ab6bd3
Georgette Bashour <13901057@student.uts.edu.au>
2024-09-18 16:12:29 +1000

    Changed Index (default) to be Landing page. Renamed existing Index to Home

--- food-for-thought/app/_layout.tsx
+++ food-for-thought/app/_layout.tsx
@@ -5,11 +5,11 @@ export default function RootLayout() {
   return (
       <GestureHandlerRootView style={{flex: 1}}>
           <Stack>
-            <Stack.Screen name="index" options={{headerShown:false,fullScreenGestureEnabled:true,gestureDirection:'horizontal'}}/>
+            <Stack.Screen name="index" options={{headerShown:false}}/>
             <Stack.Screen name="map" options={{headerShown:false,fullScreenGestureEnabled:true,gestureDirection:'horizontal'}}/>
             <Stack.Screen name="login" options={{headerShown:false,fullScreenGestureEnabled:true,gestureDirection:'horizontal'}}/>
             <Stack.Screen name="register" options={{headerShown:false,fullScreenGestureEnabled:true,gestureDirection:'horizontal'}}/>
-            <Stack.Screen name="landing" options={{headerShown:false}}/>
+            <Stack.Screen name="home" options={{headerShown:false,fullScreenGestureEnabled:true,gestureDirection:'horizontal'}}/>
           </Stack>
       </GestureHandlerRootView>
   );

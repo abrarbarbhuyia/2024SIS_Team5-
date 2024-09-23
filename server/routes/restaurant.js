b61e0081ed45f2f9a8bd53d29bce61dd98096f8b
tarakenishauy <92384503+tarakenishauy@users.noreply.github.com>
2024-09-23 15:47:54 +1000

    feat: get restaurant using lat & long and add to database if doesnt exist
    Co-authored-by: nj808 <nj808@users.noreply.github.com>

--- server/routes/restaurant.js
+++ server/routes/restaurant.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 const Restaurant = require('../models/restaurantModel');
 const databaseMaster = require('../databaseMaster');
@@ -13,6 +15,62 @@ router.get('/getIngredient/:restaurantId', async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/getRestaurant', async (req, res) => {
+    const latitude = -33.87941228154
+    const longitude = 151.20215135093403
+    const url = `https://api.foursquare.com/v3/places/search?ll=${latitude}%2C${longitude}&categories=13000`;
+
+    try {
+        // fetch the restaurants nearby the specified latitude and longitude location (set to UTS Building 5)
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: `${process.env.FOURSQUARE_API_KEY}`
+            }
+        });
+        
+        // iterate through each restaurant in the resulting array
+        const restaurants_array = response.data.results
+        for(let i = 0; i < restaurants_array.length; i++)
+        {
+            const restaurant = restaurants_array[i];
+            const fsq_id = restaurant.fsq_id;
+
+            // check if the restaurant matches an existing entry in the restaurantdetails database
+            const query = { restaurantId: fsq_id };
+            const result = await databaseMaster.dbOp('find', 'RestaurantDetails', { query });
+
+            // if there is no existing entry
+            if (result.length == 0)
+
+            {
+                //fetch the place details of the restaurant including location, name, telephone, hours and webstie
+                const placeDetailsUrl = `https://api.foursquare.com/v3/places/${fsq_id}?fields=tel%2Cname%2Chours%2Clocation%2Cwebsite`;
+                const response1 = await axios.get(placeDetailsUrl, {
+                    headers: {
+                        Authorization: `${process.env.FOURSQUARE_API_KEY}`
+                    }
+                });
+                const placeDetails = response1.data
+
+                //create a restaurant entry using model schema and add new entry to the RestaurantDetails colelction
+                const restaurant = new Restaurant({
+                    restaurantId: fsq_id,
+                    name: placeDetails.name,
+                    address: placeDetails.location.formatted_address,
+                    openingHours: placeDetails.hours.display,
+                    phoneNumber: parseInt(placeDetails.tel),
+                    website: placeDetails.website,
+                }) 
+                await databaseMaster.dbOp('insert', 'RestaurantDetails', { docs: [restaurant] });  
+            }
+        }  
+ 
+    } catch(error) {
+        console.error(error);
+        throw new Error('Failed to get restaurant!');
     }
 });
 

9e3741dc0134325a428d3330e5dbac016ce98657
Georgette Bashour <13901057@student.uts.edu.au>
2024-10-22 00:04:20 +1100

    Call .length for menu item matches count, update View More button routing

--- food-for-thought/components/RestaurantModal.tsx
+++ food-for-thought/components/RestaurantModal.tsx
@@ -191,11 +191,11 @@ export function RestaurantModal({ restaurant, userLocation, setShowModal, ...res
             ({restaurant.total_ratings})
           </Text>
         </View>}
-        <View style={{ marginLeft: 'auto' }}>{restaurant.menuItemMatches && <MenuItemBadge matches={restaurant.menuItemMatches} />}</View>
+        <View style={{ marginLeft: 'auto' }}>{restaurant.menuItemMatches && <MenuItemBadge matches={restaurant.menuItemMatches.length} />}</View>
       </View>
       {restaurant.menuItemMatches && <View style={styles.flexFormGroup}>
         <Text style={styles.formDescriptionText}>
-          {restaurant.menuItemMatches} menu items matches your dietary filters!
+          {restaurant.menuItemMatches.length} menu items matches your dietary filters!
         </Text>
       </View>}
       <View style={styles.flexFormGroup}>
@@ -222,7 +222,7 @@ export function RestaurantModal({ restaurant, userLocation, setShowModal, ...res
         {meals && <View style={{ paddingTop: 4 }}>
           <Text numberOfLines={2} style={{ fontSize: 14, opacity: 0.8 }}>{meals.length > 0 ? meals.map(meal => meal.name.toLocaleLowerCase()).join(', ') : '<menu item>'}.</Text></View>}
       </View>
-      <Button buttonStyle={{ ...styles.button, paddingHorizontal: 25, marginTop: 0 }} titleStyle={{ ...styles.buttonTitle, fontSize: 12 }} onPress={() => { router.push('/restaurant'); setShowModal(undefined); }} title={('view more').toUpperCase()} />
+      <Button buttonStyle={{ ...styles.button, paddingHorizontal: 25, marginTop: 0 }} titleStyle={{ ...styles.buttonTitle, fontSize: 12 }} onPress={() => { router.push({ pathname: "/restaurant", params: { restaurant: JSON.stringify(restaurant) } }); setShowModal(undefined); }} title={('view more').toUpperCase()} />
     </View>
   </Overlay>
 }

e260584e9042d5d05edd4caceac2f2223a09e2b6
Z <13918317@student.uts.edu.au>
2024-10-22 22:05:28 +1100

    Some progress, need to make home back based on token as well

--- food-for-thought/app/index.tsx
+++ food-for-thought/app/index.tsx
@@ -1,10 +1,29 @@
 import { Card } from '@rneui/themed';
 import { router } from 'expo-router';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { styles } from '../styles/app-styles'; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Index() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  const loadUser = useCallback(async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+      router.push('/home');
+    }
+  }, []);
+
+  useEffect(() => {
+    loadUser();
+  }, [loadUser]);
+
+  if (isLoggedIn) {
+    return null;
+  }
+
   return (
     <View style={styles.loginContainer}>
       <Card containerStyle={styles.rectangle}>
@@ -21,7 +40,7 @@ export default function Index() {
           <Text style={styles.signUpButtonText}>SIGN UP</Text>
         </TouchableOpacity>
 
-        <Text style={styles.guestText} onPress={() => router.push('/home')}>Continue as a guest</Text>
+        <Text style={styles.guestText} onPress={() => router.push('/home')}>Continue as a guest.</Text>
         <Text style={styles.supportingTextHome}>Your preferences won't be saved!</Text>
       </Card>
     </View>
